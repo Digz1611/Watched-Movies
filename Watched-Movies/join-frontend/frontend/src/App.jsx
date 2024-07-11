8cbb59382d031eb0418cd2e103a57e05244b3cf0
Digz1611 <diego.langeveldt@gmail.com>
2024-07-11 16:28:07 +0200

    both frontnd are ready to join, just needa sort app.jsx and main export files accordingly

--- Watched-Movies/join-frontend/frontend/src/App.jsx
+++ Watched-Movies/join-frontend/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/HomeM';
 import CreateMovies from './pages/CreateMovies';
 import ShowMovie from './pages/ShowMovie';
 import EditMovie from './pages/EditMovie';
--- Watched-Movies/join-frontend/frontend/src/pages/HomeM.jsx
+++ Watched-Movies/join-frontend/frontend/src/pages/HomeM.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import './HomeM.css';
 import MoviesTable from '../components/home/MoviesTable';
 import MoviesCard from '../components/home/MoviesCard';
 
